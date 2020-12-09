e88cc58cb0af803e20b744846019da16f6e44880
GamzeSirakaya <sirakayagamze3@gmail.com>
2020-12-09 22:22:42 +0300

    Create images.dart

---
+++ musicPlayer/lib/ui/images.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+// çark
+Widget images() {
+  return Container(
+    width: 320,
+    height: 320,
+    padding: EdgeInsets.only(top: 10, left: 68, right: 48),
+    child: Image.asset('assets/images/oynatici.png'),
+  );
+}
