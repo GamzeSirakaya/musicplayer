d9f165e92b2f469253ed75c744319f75e4f5be0a
GamzeSirakaya <sirakayagamze3@gmail.com>
2020-12-09 22:22:30 +0300

    Create orta.dart

---
+++ musicPlayer/lib/ui/orta.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Widget orta() {
+  return Column(
+    children: <Widget>[
+      ListView(
+        children: <Widget>[
+          Icon(Icons.playlist_play, color: Color(0xffEF0D73), size: 30.0)
+        ],
+      ),
+      Row(
+        children: <Widget>[
+          Text('asdfgj'),
+        ],
+      )
+    ],
+  );
+}
